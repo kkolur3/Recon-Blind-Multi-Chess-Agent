314ab8869a08868f6bb5e3f9a6689e6ac4cf6864
Keshav Kolur <keshav@Keshavs-MacBook-Pro.local>
2019-04-21 21:33:00 -0400

    changed episode training to train both on white and black sides of chess game

--- train_model.py
+++ train_model.py
@@ -7,9 +7,7 @@ import pickle
 # from google.colab import files
 
 rnn_size = 128
-discount_factor = 0.9
 alpha = 0.9
-replay_memory_size = 1000
 learning_rate = 0.001
 
 in_width = 8
@@ -27,50 +25,49 @@ q_val = tf.placeholder(tf.float32, shape=1, name="q_val")
 train_length = tf.placeholder(dtype=tf.int32)
 
 # Create CNNs
-conv_1_weights = tf.get_variable("conv_1_weights", shape=(3, 3, 1, 4),
+c1_weights = tf.get_variable("c1_weights", shape=(3, 3, 1, 4),
                                  dtype="float32", initializer=tf.contrib.layers.xavier_initializer())
-conv_1_bias = tf.get_variable("conv_1_bias", shape=4, initializer=tf.constant_initializer(0.0))
-conv_2_weights = tf.get_variable("conv_2_weights", shape=(3, 3, 4, 8),
+c1_bias = tf.get_variable("c1_bias", shape=4, initializer=tf.constant_initializer(0.0))
+c2_weights = tf.get_variable("c2_weights", shape=(3, 3, 4, 8),
                                  dtype="float32", initializer=tf.contrib.layers.xavier_initializer())
-conv_2_bias = tf.get_variable("conv_2_bias", shape=8, initializer=tf.constant_initializer(0.0))
-conv_3_weights = tf.get_variable("conv_3_weights", shape=(3, 3, 8, 16),
+c2_bias = tf.get_variable("c2_bias", shape=8, initializer=tf.constant_initializer(0.0))
+c3_weights = tf.get_variable("c3_weights", shape=(3, 3, 8, 16),
                                  dtype="float32", initializer=tf.contrib.layers.xavier_initializer())
-conv_3_bias = tf.get_variable("conv_3_bias", shape=16, initializer=tf.constant_initializer(0.0))
+c3_bias = tf.get_variable("c3_bias", shape=16, initializer=tf.constant_initializer(0.0))
 
 # Run board through CNNs
-conv1 = tf.nn.relu(tf.nn.conv2d(board, conv_1_weights, strides=[1, 1, 1, 1], padding="SAME") + conv_1_bias)
-pool1 = tf.nn.max_pool(conv1, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
+c1 = tf.nn.relu(tf.nn.conv2d(board, c1_weights, strides=[1, 1, 1, 1], padding="SAME") + c1_bias)
+pool1 = tf.nn.max_pool(c1, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
 bn1 = tf.layers.batch_normalization(pool1)
 
-conv2 = tf.nn.relu(tf.nn.conv2d(bn1, conv_2_weights, strides=[1, 1, 1, 1], padding="SAME") + conv_2_bias)
-pool2 = tf.nn.max_pool(conv2, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
+c2 = tf.nn.relu(tf.nn.conv2d(bn1, c2_weights, strides=[1, 1, 1, 1], padding="SAME") + c2_bias)
+pool2 = tf.nn.max_pool(c2, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
 bn2 = tf.layers.batch_normalization(pool2)
 
-conv3 = tf.nn.relu(tf.nn.conv2d(bn2, conv_3_weights, strides=[1, 1, 1, 1], padding="SAME") + conv_3_bias)
-pool3 = tf.nn.max_pool(conv3, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
+c3 = tf.nn.relu(tf.nn.conv2d(bn2, c3_weights, strides=[1, 1, 1, 1], padding="SAME") + c3_bias)
+pool3 = tf.nn.max_pool(c3, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
 bn3 = tf.layers.batch_normalization(pool3)
 
 # Run action through fully connected layer
-action_fc1_weights = tf.get_variable(name="action_fc1_weights", shape=[num_outputs, rnn_size], dtype="float32",
+action_weights = tf.get_variable(name="action_weights", shape=[num_outputs, rnn_size], dtype="float32",
                              initializer=tf.contrib.layers.xavier_initializer())
-action_fc1_bias = tf.get_variable(name="action_fc1_bias", shape=rnn_size, dtype='float32',
+action_bias = tf.get_variable(name="action_bias", shape=rnn_size, dtype='float32',
                                   initializer=tf.constant_initializer(0.0))
-action_fc1 = tf.nn.tanh(tf.add(tf.matmul(actions, action_fc1_weights), action_fc1_bias))
+action_layer = tf.nn.tanh(tf.add(tf.matmul(actions, action_weights), action_bias))
 
 # Combined board ran through CNN with action taken
-reshaped = tf.reshape(bn3, shape=(-1, rnn_size))
-concatenate = tf.concat([reshaped, action_fc1], axis=-1)
+batch_reshaped = tf.reshape(bn3, shape=(-1, rnn_size))
+board_action = tf.concat([batch_reshaped, action_layer], axis=-1)
 
 combined_action_board_weights = tf.get_variable(name="combined_action_board_weights", shape=(256, rnn_size),
                                                 dtype="float32", initializer=tf.contrib.layers.xavier_initializer())
 combined_action_board_bias = tf.get_variable(name="combined_action_bias", shape=rnn_size,
                                              dtype="float32", initializer=tf.constant_initializer(0.0))
-action_board_fc2 = tf.add(tf.matmul(concatenate, combined_action_board_weights), combined_action_board_bias)
+board_action_output = tf.add(tf.matmul(board_action, combined_action_board_weights), combined_action_board_bias)
 
 # Run through RNN
-flattened = tf.contrib.slim.flatten(action_board_fc2)
+flattened = tf.contrib.slim.flatten(board_action_output)
 convFlat = tf.nn.tanh(tf.reshape(flattened, [1, train_length, rnn_size]))
-# convFlat = tf.nn.tanh(tf.reshape(flattened, [1, train_length, rnn_size]))
 lstm_cell = rnn_cell.BasicLSTMCell(rnn_size)
 outputs, states = tf.nn.dynamic_rnn(lstm_cell, convFlat, dtype="float32",
                                     initial_state=(rnn_cell.LSTMStateTuple(hidden_state, cell_state)))
@@ -89,11 +86,7 @@ sess.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
 
 
-def softmax(x):
-    return 1.0 / (1 + np.exp(-x))
-
-
-def forward_pass(input_tensor, action_tensor, hidden_state_tensor, cell_state_tensor):
+def input_pass(input_tensor, action_tensor, hidden_state_tensor, cell_state_tensor):
     probs, hidden = sess.run([logits, states], feed_dict={board: input_tensor,
                                                           actions: action_tensor,
                                                           hidden_state: hidden_state_tensor,
@@ -105,24 +98,23 @@ def forward_pass(input_tensor, action_tensor, hidden_state_tensor, cell_state_te
 def init_dist(color):
     #encoding = [color, pawn, knight, bishop, rook, queen, king]
     dist = np.zeros(shape=(64, 7))
-    isWhite = (color == chess.WHITE)
     for i in range(64):
         if i == 0 or i == 7 or i == 56 or i == 63: # ROOK
-            square_dist = [isWhite, 0, 0, 0, 1, 0, 0]
+            square_dist = [0, 0, 0, 0, 1, 0, 0]
         elif i == 1 or i == 6 or i == 57 or i == 62: # KNIGHT
-            square_dist = [isWhite, 0, 1, 0, 0, 0, 0]
+            square_dist = [0, 0, 1, 0, 0, 0, 0]
         elif i == 2 or i == 5 or i == 58 or i == 61: # BISHOP
-            square_dist = [isWhite, 0, 0, 1, 0, 0, 0]
+            square_dist = [0, 0, 0, 1, 0, 0, 0]
         elif i == 3 or i == 59: # QUEEN
-            square_dist = [isWhite, 0, 0, 0, 0, 1, 0]
+            square_dist = [0, 0, 0, 0, 0, 1, 0]
         elif i == 4 or i == 60: # KING
-            square_dist = [isWhite, 0, 0, 0, 0, 0, 1]
+            square_dist = [0, 0, 0, 0, 0, 0, 1]
         elif (i > 7 and i < 16) or (i > 47 and i < 56):
-            square_dist = [isWhite, 1, 0, 0, 0, 0, 0]
+            square_dist = [0, 1, 0, 0, 0, 0, 0]
         else:
             square_dist = [0, 0, 0, 0, 0, 0, 0]
-        if i > 47:
-            square_dist[0] = square_dist[not isWhite]
+        if (i < 16 and color == chess.WHITE) or (i > 47 and color == chess.BLACK):
+            square_dist[0] = 1
         dist[i] = square_dist
     return dist
 
@@ -134,7 +126,8 @@ def create_episodes():
     from my_agent import StateEncoding
     for color in [chess.WHITE, chess.BLACK]:
         state = StateEncoding(color)
-
+        if color == chess.BLACK:
+            state = StateEncoding(color)
         game_history_dir = os.getcwd() + "/GameHistory"
         if os.path.isfile(os.path.join(os.getcwd(), "/episodes.pkl")):
             with (open("episodes.pkl", "rb")) as f:
@@ -142,7 +135,7 @@ def create_episodes():
         else:
             episodes = dict()
         prevBoardDist = init_dist(color)
-        action_tensor = np.zeros(shape=(1, 64*82)) # chess.move from uci method that u pass in move string
+        action_tensor = np.zeros(shape=(1, 64*82))
         hidden_stat = np.zeros((1, rnn_size))
         cell_stat = np.zeros((1, rnn_size))
         reward = 0.0
@@ -221,7 +214,7 @@ def create_episodes():
                             action = state.create_move_encoding(uciMove)
                             state.update_state_with_move(uciMove, False, False)
                             reward = state.compute_reward()
-                            probs, hidden = forward_pass(prevBoardDist.reshape((1, 64, 7, 1)),
+                            probs, hidden = input_pass(prevBoardDist.reshape((1, 64, 7, 1)),
                                                          np.array(action_tensor), hidden_stat, cell_stat)
                             episode.append({"prevBoard": prevBoardDist, "action": action, "reward": reward,
                                              "curBoard": boardDist, "hidden": hidden.h, "cell": hidden.c})
@@ -235,7 +228,7 @@ def create_episodes():
                         episodes[filenamePlayer] = episode
                     else:
                         os.remove(os.path.join(game_history_dir, filename))
-    f = open("episodesCopy.pkl", "wb")
+    f = open("episodes.pkl", "wb")
     pickle.dump(episodes, f)
     f.close()
     # return episodes
@@ -250,7 +243,7 @@ def train_network(iterations):
     for episode in episodes:
         print(episode)
         for i in range(iterations):
-            lo_sum = np.array([0.0])
+            totalLoss = np.array([0.0])
 
             prevBoards = []
             moves = []
@@ -274,22 +267,22 @@ def train_network(iterations):
             final_hidden = hidden[-1]
             final_cell = hidden[-1]
 
-            probs, hidden = forward_pass(np.array(final_obs).reshape((1, 64, 7, 1)),
+            probs, hidden = input_pass(np.array(final_obs).reshape((1, 64, 7, 1)),
                                          np.array(final_move).reshape((1, 64*82)), final_cell, final_hidden)
             q_value = final_reward + alpha * np.max(probs)
             num_turns = len(episodes[episode])
 
-            lo, _ = sess.run([loss, train], feed_dict={board: np.array(curBoards).reshape((num_turns, 64, 7, 1)),
+            curLoss, _ = sess.run([loss, train], feed_dict={board: np.array(curBoards).reshape((num_turns, 64, 7, 1)),
                                                               actions: np.array(moves),
                                                               hidden_state: initial_hidden,
                                                               cell_state: initial_cell,
                                                               q_val: np.array([q_value]),
                                                               train_length: num_turns})
-            lo_sum += lo
+            totalLoss += curLoss
 
     saver.save(sess, "model/cur_model.ckpt") # CHANGE TO EITHER PREV OR CUR
     # files.download("model/prev_model.ckpt.meta")
-    print("Loss: %d" % lo_sum)
+    print("Loss: %d" % totalLoss)
 
 letter_to_row = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
 
@@ -310,7 +303,7 @@ def make_move(state, possible_moves):
     beliefBoard = np.zeros((1, rnn_size)) #state.dists
     for move in possible_moves:
         action = np.array(state.create_move_encoding(move)).reshape(1, 5248)
-        probs, hidden = forward_pass(boardDist, action, observedBoard, beliefBoard)
+        probs, hidden = input_pass(boardDist, action, observedBoard, beliefBoard)
         state_out = [hidden.c[:1, :], hidden.h[:1, :]]
         current_reward = state.compute_reward()
         action_reward = state.compute_move_reward_change(move)
